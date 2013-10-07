94597d243ca98198f0ac3e3b2e1b3328c985a621
Michael Limiero <mike5713@gmail.com>
2013-10-07 01:06:29 -0400

    Simple hopper protection

--- src/main/java/net/miscjunk/fancyshop/FancyShop.java
+++ src/main/java/net/miscjunk/fancyshop/FancyShop.java
@@ -5,12 +5,14 @@ import lib.PatPeter.SQLibrary.SQLite;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -130,6 +132,19 @@ public class FancyShop extends JavaPlugin implements Listener {
         if (!canBeShop(event.getBlock())) return;
         Inventory inv = ((InventoryHolder)event.getBlock().getState()).getInventory();
         if (Shop.isShop(inv)) event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (event.getBlock().getType() != Material.HOPPER) return;
+        Block above = event.getBlock().getRelative(BlockFace.UP);
+        if (!canBeShop(above)) return;
+        Inventory inv = ((InventoryHolder)above.getState()).getInventory();
+        if (!Shop.isShop(inv)) return;
+        Shop shop = Shop.fromInventory(inv, "");
+        if (shop.getOwner().equals(event.getPlayer().getName())) return; // we'll assume they know what they're doing
+        event.setCancelled(true);
+        Chat.e(event.getPlayer(), "You can't place that here.");
     }
 
     private boolean canBeShop(Block block) {
