6b980fa9d3210b56d4559236b50ca942727d4752
Michael Limiero <mike5713@gmail.com>
2013-10-07 00:36:28 -0400

    Protect opening shop chests

--- src/main/java/net/miscjunk/fancyshop/FancyShop.java
+++ src/main/java/net/miscjunk/fancyshop/FancyShop.java
@@ -4,6 +4,7 @@ import lib.PatPeter.SQLibrary.Database;
 import lib.PatPeter.SQLibrary.SQLite;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -33,25 +34,26 @@ public class FancyShop extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getMaterial() == Material.STICK) {
-            if (event.getClickedBlock() != null && event.getClickedBlock().getType() == Material.CHEST) {
-                event.setCancelled(true);
-                Shop shop = Shop.fromInventory(((InventoryHolder)event.getClickedBlock().getState()).getInventory(),
-                        event.getPlayer().getName());
-                shop.open(event.getPlayer());
+        if (!canBeShop(event.getClickedBlock()) || event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        Inventory inv = ((InventoryHolder)event.getClickedBlock().getState()).getInventory();
+        if (event.getPlayer().isSneaking()) {
+            if (Shop.isShop(inv)) {
+                Shop shop = Shop.fromInventory(inv, event.getPlayer().getName());
+                if (!shop.getOwner().equals(event.getPlayer().getName()) && !event.getPlayer().hasPermission("fancyshop.open")) {
+                    event.setCancelled(true);
+                    Chat.e(event.getPlayer(), "You don't have permission to open this shop chest.");
+                }
             }
-        } else if (event.getAction() == Action.RIGHT_CLICK_BLOCK && !event.getPlayer().isSneaking()
-                && event.getClickedBlock() != null && event.getClickedBlock().getState() instanceof InventoryHolder) {
+        } else {
             Player p = event.getPlayer();
             if (cmdExecutor.hasPending(p)) {
                 cmdExecutor.onPlayerInteract(event);
             } else {
-                Inventory inv = ((InventoryHolder)event.getClickedBlock().getState()).getInventory();
                 if (Shop.isShop(inv)) {
                     event.setCancelled(true);
                     if (p.hasPermission("fancyshop.use")) {
                         Shop shop = Shop.fromInventory(inv, p.getName());
-                        if (p.getName().equals(shop.getOwner())) {
+                        if (p.getName().equals(shop.getOwner()) && event.getMaterial() != Material.STICK) {
                             shop.edit(p);
                         } else {
                             shop.open(p);
@@ -86,7 +88,7 @@ public class FancyShop extends JavaPlugin implements Listener {
     public void onInventoryClose(InventoryCloseEvent event) {
         if (Shop.isShop(event.getInventory())) {
             // update shop view after using chest
-            Shop shop = Shop.fromInventory(event.getInventory(),"");
+            Shop shop = Shop.fromInventory(event.getInventory(), "");
             shop.refreshView();
             shop.refreshEditor();
         } else if (event.getInventory().getHolder() instanceof ShopEditor) {
@@ -94,6 +96,11 @@ public class FancyShop extends JavaPlugin implements Listener {
             Shop shop = ((ShopEditor)event.getInventory().getHolder()).getShop();
             ShopRepository.store(shop);
         }
+    }
+
+    private boolean canBeShop(Block block) {
+        if (block == null) return false;
+        return block.getState() instanceof InventoryHolder;
     }
 }
 
